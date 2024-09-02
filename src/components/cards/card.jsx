0901568a8fd97dea90957ca7221f912b7e6d137c
Shaurya Priyadarshi <shpr24680@gmail.com>
2024-09-02 21:42:41 +0530

    card fix

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, CardMedia, CardContent, Typography, Button, IconButton } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ShareIcon from '@mui/icons-material/Share';
 import { useTheme } from '@mui/material/styles';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { hover } from '@testing-library/user-event/dist/hover';
+import { Diversity1 } from '@mui/icons-material';
 // Action to add item (assuming you have this in your Redux slice)
 const addItem = (item) => ({
   type: 'ADD_ITEM',
@@ -25,29 +27,51 @@ const AnimeCard = ({title, imageURL,  date, time, rating, onClick  }) => {
     // Handle share functionality here
   };
 
+  const [hovered, setHovered] = useState(false);
   return (
-    <Card sx={{ minWidth: 300, maxWidth: 300 , margin: '1rem 1rem 1rem 1rem', backgroundColor: '#0b1419'}}>
+    <Card sx={{ minWidth: 300, maxWidth: 300 , margin: '1rem 1rem 1rem 1rem', backgroundColor: '#0b1419',
+      position: 'relative',
+      transition: 'transform 0.3s ease-in-out, height 0.3s ease-in-out',
+      overflow: 'hidden',
+      height: hovered ? 300 : 150, // Adjust height based on hover state
+      '&:hover': {
+        transform: 'scale(1.1)',
+      },
+    }}
+    onMouseEnter={() => setHovered(true)}
+    onMouseLeave={() => setHovered(false)}
+    >
+
       <CardMedia
         component="img"
-        height= "140"
+        height= "150"
         image={imageURL} // Update this to your image path or a URL
         alt={title}
-      />
-      <CardContent sx={{padding: '0.5rem'}}>
+        />
+
+        
+   
+      <CardContent
+      className="card-expanded-info"
+      sx={{padding: '0.5rem',
+        // position: 'absolute',
+        transition: 'transform 0.3s ease-in-out',
+            transform: hovered ? 'translateY(0)' : 'translateY(100%)',
+       }}>
         <Typography  gutterBottom sx={{fontSize: '0.9rem'}} component="div">
           {title}
 
         </Typography>
         <Button  style={{
-        width: '95%',
-        backgroundColor: '#D1D1D1',
-        color: '#071116',
-        fontWeight: 'bold',
-        alignContent: 'center',
-        justifyContent: 'center',
-        fontSize: '1rem',
-
-      }} >
+          width: '95%',
+          backgroundColor: '#D1D1D1',
+          color: '#071116',
+          fontWeight: 'bold',
+          alignContent: 'center',
+          justifyContent: 'center',
+          fontSize: '1rem',
+          
+        }} >
         <PlayArrowIcon sx={{ marginRight: '0.3rem', fontSize: '1.5rem'}}/>
           Play
         </Button>
@@ -55,7 +79,11 @@ const AnimeCard = ({title, imageURL,  date, time, rating, onClick  }) => {
 
       </CardContent>
 
-      <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0.1rem 0.1rem 0.1rem 0.9rem' }}>
+      <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0.1rem 0.1rem 0.1rem 0.9rem',
+      // position: 'absolute',
+      transition: 'transform 0.3s ease-in-out',
+          transform: hovered ? 'translateY(0)' : 'translateY(100%)',
+      }}>
         <div>
           <IconButton onClick={handleAddItem}>
             <AddCircleOutlineIcon sx={{fontSize: '1.5rem'}} />
@@ -66,11 +94,16 @@ const AnimeCard = ({title, imageURL,  date, time, rating, onClick  }) => {
         </div>
         
       </CardContent>
-      <CardContent sx={{padding: '0.1rem 0.1rem 0.1rem 0.1rem'}}>
+      <CardContent sx={{padding: '0.1rem 0.1rem 0.1rem 0.1rem',
+        // position: 'absolute',
+        transition: 'transform 0.3s ease-in-out',
+            transform: hovered ? 'translateY(0)' : 'translateY(100%)',
+      }}>
       <Typography sx={{display: 'flex', fontSize: '0.8rem', padding: '0.1rem 0.1rem 0.1rem 1rem'}} color="text.secondary">
           {date} | {time} | {rating}
         </Typography>
       </CardContent>
+            
 
       
     </Card>
