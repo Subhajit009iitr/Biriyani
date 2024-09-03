2573e90989113eadf2c99f27df44bd7a4aae18e2
Subhajit009iitr <biswassubhajit009@gmail.com>
2024-09-03 15:05:52 +0530

    Remove Warnings

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Cover from "../components/covers/cover";
 import HomeBody from '../components/covers/homeBody';
 import NavBar from '../components/navbars/nav';
 
 function Home() {
-  const theme = useTheme();
 
   return (
     <div>
