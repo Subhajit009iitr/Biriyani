563bb6c4bd5e5b086fffefe0e70f5bf812da3dff
Subhajit009iitr <biswassubhajit009@gmail.com>
2024-09-03 12:38:10 +0530

    Code cleanup

--- src/components/scrollers/scroller.jsx
+++ src/components/scrollers/scroller.jsx
@@ -5,7 +5,6 @@ import { ScrollLeftButton, ScrollRightButton } from '../buttons/scroll';
 import ScrollableCard from '../cards/card';
 import image5 from '../../assets/pic5.png';
 
-// Horizontal Scrollable Container with Buttons
 function HorizontalScrollable() {
   const theme = useTheme();
   const scrollContainerRef = useRef(null);
@@ -42,80 +41,18 @@ function HorizontalScrollable() {
   }, []);
 
   const cards = [
-    {
-      title: 'Card 1',
-      imageURL: image5,
-      date: '2024-09-02',
-      time: '10:00 AM',
-      rating: 4.5
-    },
-    {
-      title: 'Card 2',
-      imageURL: image5,
-      date: '2024-09-03',
-      time: '11:00 AM',
-      rating: 4.0
-    },
-    {
-      title: 'Card 3',
-      imageURL: image5,
-      date: '2024-09-04',
-      time: '12:00 PM',
-      rating: 4.2
-    },
-    {
-      title: 'Card 4',
-      imageURL: image5,
-      date: '2024-09-05',
-      time: '01:00 PM',
-      rating: 4.7
-    },
-    {
-      title: 'Card 5',
-      imageURL: image5,
-      date: '2024-09-06',
-      time: '02:00 PM',
-      rating: 4.9
-    },
-    {
-      title: 'Card 1',
-      imageURL: image5,
-      date: '2024-09-02',
-      time: '10:00 AM',
-      rating: 4.5
-    },
-    {
-      title: 'Card 2',
-      imageURL: image5,
-      date: '2024-09-03',
-      time: '11:00 AM',
-      rating: 4.0
-    },
-    {
-      title: 'Card 3',
-      imageURL: image5,
-      date: '2024-09-04',
-      time: '12:00 PM',
-      rating: 4.2
-    },
-    {
-      title: 'Card 4',
-      imageURL: image5,
-      date: '2024-09-05',
-      time: '01:00 PM',
-      rating: 4.7
-    },
-    {
-      title: 'Card 5',
-      imageURL: image5,
-      date: '2024-09-06',
-      time: '02:00 PM',
-      rating: 4.9
-    }
-    
-    
-    // Add more cards as needed
+    { title: 'Card 1', imageURL: image5, date: '2024-09-02', time: '10:00 AM', rating: 4.5 },
+    { title: 'Card 2', imageURL: image5, date: '2024-09-03', time: '11:00 AM', rating: 4.0 },
+    { title: 'Card 3', imageURL: image5, date: '2024-09-04', time: '12:00 PM', rating: 4.2 },
+    { title: 'Card 4', imageURL: image5, date: '2024-09-05', time: '01:00 PM', rating: 4.7 },
+    { title: 'Card 5', imageURL: image5, date: '2024-09-06', time: '02:00 PM', rating: 4.9 },
+    { title: 'Card 1', imageURL: image5, date: '2024-09-02', time: '10:00 AM', rating: 4.5 },
+    { title: 'Card 2', imageURL: image5, date: '2024-09-03', time: '11:00 AM', rating: 4.0 },
+    { title: 'Card 3', imageURL: image5, date: '2024-09-04', time: '12:00 PM', rating: 4.2 },
+    { title: 'Card 4', imageURL: image5, date: '2024-09-05', time: '01:00 PM', rating: 4.7 },
+    { title: 'Card 5', imageURL: image5, date: '2024-09-06', time: '02:00 PM', rating: 4.9 }
   ];
+
   return (
     <Box position="relative" display="flex" alignItems="center">
       {canScrollLeft && <ScrollLeftButton onClick={scrollLeft} />}
